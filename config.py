7f38ab9be6b951e7e7916b60f08cc865ae1de94b
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2021-09-21 16:35:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class vars(object):
-    api_id = os.getenv('API_ID', 123456)
-    api_hash = os.getenv('API_HASH' ,'abcdefghxxxxx')
-    bot_token = os.getenv('BOT_TOKEN', None)
+    api_id = os.getenv('API_ID', 3881325)
+    api_hash = os.getenv('API_HASH' ,'1ebf12e67222683abe1c01f0209b79df')
+    bot_token = os.getenv('BOT_TOKEN', 2029085800:AAEljS2Sm1Y3psbe3Ksk_xuOAIVg_HHJImA)
     max = int(os.getenv('MAX', 20))
